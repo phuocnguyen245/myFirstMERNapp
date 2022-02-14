23bc729b55c8d01c4a4f1685f75d0e5e33c997d7
p.nguyen <phuocnguyen24598@gmail.com>
2022-02-15 01:04:50 +0700

    save

--- Client/src/components/homepage/SearchBar.js
+++ Client/src/components/homepage/SearchBar.js
@@ -1,17 +1,21 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
 const SearchBar = () => {
+    const shops = useSelector(state => state.homepage.searchData)
     return (
         <ul className="search-bar">
-            <li>
+            {shops.map(shop => {
+                return <li>
                 <a href="/#">
-                    <img src="" alt="" />
+                    <img src={`./assets/img/${shop.img}`} alt="" />
                     <div className="search-bar__item">
-                        <p>Hihi</p>
-                        <p>Cost</p>
+                        <p>{shop.shopName}</p>
+                        <p>{shop.address}</p>
                     </div>
                 </a>
             </li>
+            })}
+            
         </ul>
     )
 }
