0959344a6310a1e634f32a57988a8bdccf3adea9
p.nguyen <phuocnguyen24598@gmail.com>
2022-02-25 09:23:29 +0700

    save

--- Client/src/redux/sagas/index.js
+++ Client/src/redux/sagas/index.js
@@ -3,16 +3,18 @@ import axios from 'axios'
 import { getCategoriesSuccess, getShopSuccess, getSearchSuccess } from '../../components/homepage/homePageSlice'
 
 function* getData(action) {
-    const cats = yield call(() => axios.get('http://localhost:5000/api/homepage/'))
-    const formattedCats = yield cats.data
-    yield put(getCategoriesSuccess(formattedCats.categories))
-    yield put(getShopSuccess(formattedCats.shops))
+    const homepageApi = yield call(() => axios.get('http://localhost:5000/api/homepage/'))
+    const data = yield homepageApi.data
+    yield put(getCategoriesSuccess(data.categories))
+    yield put(getShopSuccess(data.shops))
 }
+
 function* searchData(action) {
     const searchApi = yield call(() => axios.get(`http://localhost:5000/api/search?q=${action.payload}`))
     const data = yield searchApi.data
     yield put(getSearchSuccess(data.shops))
 }
+
 function* mySaga() {
     yield takeEvery('homepage/getHomepageDataFetch', getData)
     yield takeEvery('homepage/getSearchFetch', searchData)
